1dbc4de61b496b704b23ecf90bc923b0e256b78f
Migheli <98918370+Migheli@users.noreply.github.com>
2022-03-08 12:08:50 +0300

    Исправлены настройки ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,8 @@ INSTALLED_APPS = ['datacenter']
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 ROOT_URLCONF = 'project.urls'
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+print(env.list('ALLOWED_HOSTS'))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES = [
     {
